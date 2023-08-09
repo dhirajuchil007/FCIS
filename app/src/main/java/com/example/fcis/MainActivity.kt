c4abfb77b7b9b9278949c7fb6f28b44e8a60d90e
kettycoot <p2434636@my365.dmu.ac.uk>
2023-08-09 21:49:44 +0100

    created login page & sign-in page

--- app/src/main/java/com/example/fcis/MainActivity.kt
+++ app/src/main/java/com/example/fcis/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.fcis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    fun btnLogin(view: View) {
+        val Message = findViewById<TextView>(R.id.textViewMessage)
+        val Username = findViewById<EditText>(R.id.etUsername).text.toString()
+        val Password = findViewById<EditText>(R.id.etPassword).text.toString()
+
+
+            val btnLogin = findViewById<Button>(R.id.btnLogin)
+            btnLogin.setOnClickListener {
+                val Intent = Intent(this, HomePage::class.java)
+                startActivity(Intent)
+            }
+            if (Username != Password)
+                Toast.makeText(this, "Wrong Username or Password", Toast.LENGTH_LONG).show()
+
+        }
+
+   fun btn_Sign_in_Options(view: View) {
+       val btn_Sign_in_Options = findViewById<Button>(R.id.btn_Sign_in_Options)
+       btn_Sign_in_Options.setOnClickListener {
+           val Intent = Intent(this, Sign_in_options::class.java)
+           startActivity(Intent)
+       }
+   }
     }
 }
---
+++ app/src/main/java/com/example/fcis/Sign_in_options.kt
@@ -0,0 +1,8 @@
+package com.example.fcis
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+class Sign_in_options : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_options)
+    }}
