45729375f698cfad8936cd003475775f15a258e1
kettycoot <p2434636@my365.dmu.ac.uk>
2023-09-05 22:12:03 +0100

    created login page & sign-in page

--- app/src/main/java/com/example/fcis/SignInOptionsPage.kt
+++ app/src/main/java/com/example/fcis/SignInOptionsPage.kt
@@ -2,9 +2,14 @@ package com.example.fcis
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -14,9 +19,8 @@ class SignInOptionsPage : AppCompatActivity() {
     // 1 button
     var signUp: Button? = null
 
-    // 3 texts fields
+    // 7 texts fields
     var fullName: EditText? = null
-    var roleType: EditText? = null
     var password: EditText? = null
     var branchType : TextView? = null
     var txtSignIn: TextView? = null
@@ -26,8 +30,24 @@ class SignInOptionsPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in_options_page)
 
+        val roleType_spinner = findViewById<Spinner>(R.id.roleTypeSpinner)
+        val arrayAdapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.role_type,
+            android.R.layout.simple_spinner_item
+        )
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = arrayAdapter
+        spinner.onItenSelectListener = this
+
+    override fun onItemSelected(parent: adapterView<*>?, view: View?, position:int, id:Long) {
+         val text= adapterView?.getItemAtPosition(position).toString ()     }
+    override fun onNothingSelected(parent: AdapterView<*>? ){
+        TODO("Not yet implemented")
+    }
+
+
         fullName=findViewById(R.id.edtSignUpFullName)
-        roleType =findViewById(R.id.edtSignUpFullName)
         branchType =findViewById(R.id.edtSignUpFullName)
         email =findViewById(R.id.edtSignUpEmail)
         password =findViewById(R.id.edtSignUpPassword)
